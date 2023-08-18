c3433c9a95be6ed224923af5e8920565d2519a0f
Alisson <alisson-sds@hotmail.com>
2023-08-18 13:05:53 -0300

    Updating show-sql in properties

--- src/main/java/br/fynansis/Fynansis/configs/WebConfig.java
+++ src/main/java/br/fynansis/Fynansis/configs/WebConfig.java
@@ -10,7 +10,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
 
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("http://localhost:5173");
+        registry.addMapping("/**").allowedOrigins("http://localhost:5173")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
     }
 
 }
